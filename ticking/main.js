e980b929590e95afe224cff2809d019828b72cc7
chaoui940 <daging129@gmail.com>
2022-01-31 19:02:14 +0100

    *

--- ticking/main.js
+++ ticking/main.js
@@ -2,7 +2,7 @@ function tick() {
     let element = (
     <div>
         <h1>Hi</h1>
-        <h2>it is {new Date().toLocaleDateString()}</h2>
+        <h2>it is {new Date().toLocaleDateString()}.</h2>
     </div>
     );
     ReactDOM.render(element , document.getElementById('root'));
